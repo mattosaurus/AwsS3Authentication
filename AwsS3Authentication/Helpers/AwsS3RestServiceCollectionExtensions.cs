4fb43d6403a4b380e7487c9265defafd97569aa4
mattosaurus <bitscry@outlook.com>
2020-09-18 10:49:39 +0100

    Add service collection extension

---
+++ AwsS3Authentication/Helpers/AwsS3RestServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwsS3Authentication.Helpers
+{
+    public static class AwsS3RestServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAwsRestClient(this IServiceCollection collection, string accessKey, string secretKey)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            RestClient restClient = new RestClient();
+            restClient.Authenticator = new V4Authenticator(true, accessKey, secretKey);
+
+            return collection.AddSingleton<IRestClient>(restClient);
+        }
+    }
+}
